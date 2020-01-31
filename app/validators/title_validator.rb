e7d225504f0d5a5ae4b2641cacb1f52d777c9f31
James Mobus <mobusj@live.com>
2020-01-31 14:57:12 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -2,6 +2,8 @@ class TitleValidator < ActiveRecord::Validator
   #sufficiently clickbait-y. If the title does not contain "Won't Believe",
 #Secret", "Top [number]", or "Guess", the validator should add a validation.
   def validate(record)
-    if record.title.match?("Won't Believe")
+    if record.title.match?(/Won't Believe|Secret|Top [0123456789]|Guess/)
+      
+    end 
   end
 end
