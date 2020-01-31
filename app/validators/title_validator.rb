404744697328dd9eccdb4be54667e8c3d17bd290
James Mobus <mobusj@live.com>
2020-01-31 14:59:30 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -2,7 +2,7 @@ class TitleValidator < ActiveModel::Validator
   #sufficiently clickbait-y. If the title does not contain "Won't Believe",
 #Secret", "Top [number]", or "Guess", the validator should add a validation.
   def validate(record)
-    if record.title.match?(/Won't Believe|Secret|Top [0123456789]|Guess/)
+    if record.title.match?(/Won't Believe|Secret|Top [0123456789]|Guess|True Facts/)
       record.errors[:title] << "No clickbait allowed!"
     end 
   end
